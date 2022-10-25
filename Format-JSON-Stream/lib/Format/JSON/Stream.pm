b2729884f8ad29dc1678f7e7839870847034bb0c
Shlomi Fish <shlomif@shlomifish.org>
2022-10-25 05:46:42 +0300

    add

---
+++ Format-JSON-Stream/lib/Format/JSON/Stream.pm
@@ -0,0 +1,26 @@
+package Format::JSON::Stream;
+
+use 5.014;
+use strict;
+use warnings;
+
+1;
+
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head2
+
+=head2
+
+=cut
+
